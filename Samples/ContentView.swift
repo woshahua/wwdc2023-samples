2c56aef091210eac18b024d99399165cfd991a7d
woshahua <wohangye@gmail.com>
2023-06-06 18:29:56 +0900

    Initial Commit

---
+++ Samples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Samples
+//
+//  Created by gaohang on 2023/06/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Samples/SamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamplesApp.swift
+//  Samples
+//
+//  Created by gaohang on 2023/06/06.
+//
+
+import SwiftUI
+
+@main
+struct SamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
